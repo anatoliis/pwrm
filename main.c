b6a14a1a63b1f358a511cdd529d48f0648d95f87
Anatolii Sukovach <anatoliy.sukovach@djangostars.com>
2019-09-05 03:22:27 +0300

    Refactoring

--- main.c
+++ main.c
@@ -7,17 +7,18 @@
 
 #define BUFFER_SIZE 80
 #define PATH "/sys/class/powercap/intel-rapl:1/energy_uj"
+#define DEFAULT_SLEEP 0.85
 
-struct measurement {
+struct measurement_t {
     float power;
     int64_t last_value;
     struct timespec measure_time;
 };
 
-void measure(struct measurement * device);
+void measure(struct measurement_t * measurement);
 
 int main(int argc, char ** argv) {
-    float sleep = 0.85;
+    float sleep = DEFAULT_SLEEP;
     if (argc == 2) {
         float arg_sleep = atof(argv[1]);
         if (!arg_sleep) {
@@ -30,33 +31,33 @@ int main(int argc, char ** argv) {
         }
         sleep = arg_sleep;
     } else if (argc > 2) {
-        printf("%s\n", "Error: expected single argument");
+        printf("%s\n", "Error: expected a single argument");
         return 1;
     }
-    struct measurement * device = malloc(sizeof(struct measurement));
+    struct measurement_t * measurement = malloc(sizeof(struct measurement_t));
     struct timespec sleep_spec;
 
-    device->power = 0;
-    device->last_value = 0;
-    device->measure_time.tv_sec = 0;
-    device->measure_time.tv_nsec = 0;
+    measurement->power = 0;
+    measurement->last_value = 0;
+    measurement->measure_time.tv_sec = 0;
+    measurement->measure_time.tv_nsec = 0;
 
     sleep_spec.tv_sec = (time_t) sleep;
     sleep_spec.tv_nsec = (suseconds_t) ((sleep - sleep_spec.tv_sec)	* 1000000000.);
 
-    measure(device);
+    measure(measurement);
     nanosleep(&sleep_spec, NULL);
-    measure(device);
+    measure(measurement);
 
-    if (device->power == 0) {
+    if (measurement->power == 0) {
         nanosleep(&sleep_spec, NULL);
-        measure(device);
+        measure(measurement);
     }
 
-    printf("%4.02f W\n", device->power);
+    printf("%4.02f W\n", measurement->power);
 }
 
-void measure(struct measurement * device) {
+void measure(struct measurement_t * measurement) {
     char buffer[BUFFER_SIZE];
     int file_descriptor = open(PATH, O_RDONLY);
     if (file_descriptor >= 0) {
@@ -67,23 +68,23 @@ void measure(struct measurement * device) {
             buffer[buffer[size - 1] == '\n' ? size - 1 : size] = '\0';
             int64_t value = (int64_t) atoll(buffer);
             clock_gettime(CLOCK_MONOTONIC, &time_now);
-            if (device->last_value > 0 && value >= device->last_value) {
+            if (measurement->last_value > 0 && value >= measurement->last_value) {
                 struct timespec time_diff;
                 int64_t diff;
-                time_diff.tv_sec = time_now.tv_sec - device->measure_time.tv_sec;
-                time_diff.tv_nsec = time_now.tv_nsec - device->measure_time.tv_nsec;
+                time_diff.tv_sec = time_now.tv_sec - measurement->measure_time.tv_sec;
+                time_diff.tv_nsec = time_now.tv_nsec - measurement->measure_time.tv_nsec;
                 while (time_diff.tv_nsec < 0) {
                     time_diff.tv_nsec += 1000000000;
                     time_diff.tv_sec--;
                 }
                 diff = (int64_t) (time_diff.tv_sec * 1000000000 + time_diff.tv_nsec);
-                power = (double) (value - device->last_value) * 1000 / diff;
+                power = (double) (value - measurement->last_value) * 1000 / diff;
             } else {
-                power = device->power;
+                power = measurement->power;
             }
-            device->last_value = value;
-            device->measure_time = time_now;
-            device->power = power;
+            measurement->last_value = value;
+            measurement->measure_time = time_now;
+            measurement->power = power;
         }
         close(file_descriptor);
     }
